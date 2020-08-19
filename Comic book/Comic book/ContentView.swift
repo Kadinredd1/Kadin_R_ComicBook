611185ac07a53717e505c233d698a7930bc06ca0
Kadinredd1 <67976031+Kadinredd1@users.noreply.github.com>
2020-08-19 12:02:36 -0500

    Add files via upload

---
+++ Comic book/Comic book/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Comic book
+//
+//  Created by Kadin Redd on 7/29/20.
+//  Copyright © 2020 Kadin Redd. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Adam West")
+    }
+}
+struct ContentVieW: View {
+    var body: some View {
+        Text("Alex smith")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
